b4e3a5547ecad759de93ef21604fd033264a7152
Rodrigo Cabrera <rkbrk2928@gmail.com>
2023-09-13 22:07:35 -0600

    first commit; my first app by React

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Counter from "./Counter";
+import EpisodeBrowser from "./EpisodeBrowser";
+
+export default function App() {
+  return (
+    <div className="App">
+      < EpisodeBrowser />
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js", // Archivo inicial
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
