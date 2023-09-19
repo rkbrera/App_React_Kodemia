d796bc1c6a328c6ef98b6a076d37375ab8afb101
Rodrigo Cabrera <rkbrk2928@gmail.com>
2023-09-19 17:37:21 -0600

    Insertar Pokemon

--- src/components/pokemon/PokemonBrowser.js
+++ src/components/pokemon/PokemonBrowser.js
@@ -2,82 +2,113 @@ import React, { useEffect, useState } from "react";
 import Pokemon from "../pokemon/Pokemon";
 
 export default function PokemonBrowser() {
-    const pokemonsDb = [{
-        id: 1,
-        name: "bulbasaur",
-        sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+  const pokemonsDb = [
+    {
+      id: 1,
+      name: "bulbasaur",
+      sprite:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
     },
     {
-        id: 4,
-        name: "charmander",
-        sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+      id: 4,
+      name: "charmander",
+      sprite:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
     },
     {
-        id: 7,
-        name: "squirtle",
-        sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
+      id: 7,
+      name: "squirtle",
+      sprite:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
     },
     {
-        id: 25,
-        name: "pikachu",
-        sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-    }];
+      id: 25,
+      name: "pikachu",
+      sprite:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+    },
+  ];
 
-    const [pokemon, setPokemon] = useState(0);
-    const [pokemons, setPokemons] = useState([]);
-    const [activePokemon, setActivePokemon] = useState({});
+  const [pokemon, setPokemon] = useState(0);
+  const [pokemons, setPokemons] = useState([]);
+  const [activePokemon, setActivePokemon] = useState({});
 
-    useEffect(() => {
-        console.log("Render")
-        setPokemons(pokemonsDb);
-    }, []);
+  useEffect(() => {
+    console.log("Render");
+    setPokemons(pokemonsDb);
+  }, []);
 
-    function nextPokemon() {
-        if (pokemon > pokemons.length - 1) {
-            alert("No more Pokemons");
-            return;
-        }
-        setPokemon(pokemon + 1);
+  function nextPokemon() {
+    if (pokemon > pokemons.length - 1) {
+      alert("No more Pokemons");
+      return;
+    }
+    setPokemon(pokemon + 1);
+  }
+
+  function previousPokemon() {
+    if (pokemon == 0) {
+      alert("No more Pokemons");
+      return;
+    }
+    setPokemon(pokemon - 1);
+  }
+
+  function insertPokemon(event) {
+    event.preventDefault()
+    const pokemonId = Number(document.getElementById("pokemonId").value)
+    const pokemonName = document.getElementById("pokemonName").value;
+    const pokemonSprite = document.getElementById("pokemonSprite").value;
+    
+    const newPokemon = {
+      id: pokemonId,
+      name: pokemonName,
+      sprite: pokemonSprite,
     }
 
-    function previousPokemon() {
-        if (pokemon == 0) {
-            alert("No more Pokemons");
-            return;
-        }
-        setPokemon(pokemon - 1);
-    }
+    setPokemons([...pokemons, newPokemon]) // Tomamos una copia del arreglo que ya esta, y lo unimos con el del nuevo pokemon
+    console.log(pokemons)
+   
+  }
 
-    function searchPokemon(){
-        const pokemonName = document.getElementById("pokemonName").value;
-        console.log(pokemonName);
 
-        const pokemonFound = pokemonsDb.find(pokemon => pokemon.name.toLowerCase() === pokemonName.toLowerCase());
-        if(pokemonFound){
-            setActivePokemon(pokemonFound);
-        }else{
-            alert("Pokemon not found");
-        }
-    }
+  return (
+    <div className="PokemonBrowser">
+      {pokemons.length > 0 ? (
+        <div>
+          <h1>Pokemon Browser</h1>
 
-    return (
-        <div className="PokemonBrowser">
-            {
-                pokemons.length > 0 ? (
-                    <div>
-                        <h1>Pokemon Browser</h1>
-                        <input id="pokemonName" type="text" placeholder="Ingresa nombre de pokemon" />
-                        <button onClick={searchPokemon}>Buscar pokemon</button>
-                        {
-                            activePokemon.name ? (
-                                <Pokemon name={activePokemon.name} id={activePokemon.id} sprite={activePokemon.sprite} />
-                            ) : (
-                                <p>No pokemon selected</p>
-                            )
-                        }
-                    </div>
-                ) : (<p>Loading...</p>)
-            }
+          <form onSubmit={insertPokemon}>
+            <input
+              id="pokemonId"
+              type="text"
+              placeholder="Pokemon ID"
+            />
+            <input
+              id="pokemonName"
+              type="text"
+              placeholder="Ingresa nombre de pokemon"
+            />
+            <input
+              id="pokemonSprite"
+              type="text"
+              placeholder="Ingresa sprite de pokemon"
+            />
+          <button onClick={insertPokemon}>Insertar</button>
+          </form>
+          {activePokemon.name ? (
+            <Pokemon
+              name={activePokemon.name}
+              id={activePokemon.id}
+              sprite={activePokemon.sprite}
+            />
+          ) : (
+            <p>No pokemon selected</p>
+          )}
         </div>
-    );
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
